0b2ef31e00a44a7d1e5f5f70d3375474b86de9c1
ruinedme <johndoe@example.com>
2023-11-30 10:37:22 -0600

    fix formatting

--- src/day4.rs
+++ src/day4.rs
@@ -16,7 +16,7 @@ pub fn run_day4(inputs: &String) {
 fn day4_1(inputs: &String) -> usize {
     let base = inputs.to_owned();
     for i in 0..u64::MAX {
-        let t = format!("{}{}",base,i);
+        let t = format!("{}{}", base, i);
         let hash = md5hash(t.as_bytes());
         if hash[0] == 0 && hash[1] == 0 && hash[2] < 10 {
             return i as usize;
@@ -30,7 +30,7 @@ fn day4_1(inputs: &String) -> usize {
 fn day4_2(inputs: &String) -> usize {
     let base = inputs.to_owned();
     for i in 0..u64::MAX {
-        let t = format!("{}{}",base,i);
+        let t = format!("{}{}", base, i);
         let hash = md5hash(t.as_bytes());
         if hash[0] == 0 && hash[1] == 0 && hash[2] == 0 {
             return i as usize;
